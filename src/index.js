28908340cc2b877d6aeacbc96b50823b21d81694
Mercy <mercymumbe17@gmail.com>
2025-03-23 21:11:22 +0300

    characters voting app with characters ids 1 and 2 not functioning

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // Your code here
+fetch ("http://localhost:3000/characters")
+
+.then (response => response.json())
+.then (data => {
+    data.forEach(character => {
+        const span = document.createElement("span");
+        span.textContent = character.name;
+        span.addEventListener("click", () => displayCharacter(character));
+        document.getElementById("character-bar").appendChild(span);
+    });
+});
+
+function displayCharacter(character) {
+    document.getElementById("name").textContent = character.name;
+    document.getElementById("image").src = character.image;
+    document.getElementById("vote-count").textContent = character.votes;
+    clickedCharacter = character;
+}
+
+let clickedCharacter;
+
+document.getElementById("votes-form").addEventListener("submit",(event) => {
+    event.preventDefault();
+    const addedVotes = parseInt(document.getElementById("votes").value);
+    if (!isNaN(addedVotes)) {
+        clickedCharacter.votes += addedVotes;
+        document.getElementById("vote-count").textContent = clickedCharacter.votes;
+        event.target.reset();
+    }
+});
+
+document.getElementById("reset-btn").addEventListener("click", () => {
+    clickedCharacter.votes = 0;
+    document.getElementById("vote-count").textContent = 0;
+});
